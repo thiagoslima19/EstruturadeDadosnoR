00a5d7772d89a5e5b817beca7c126d443157c6a9
Thiago Augusto <44952468+thiagoslima19@users.noreply.github.com>
2019-09-23 15:32:41 -0300

    Add files via upload

---
+++ EstruturaR.R
@@ -0,0 +1,16 @@
+FC<-setRefClass("FC",fields=list(MU="matrix",SU="matrix",SI="matrix"))
+t2=FC()
+alocarMU<-function(x){
+  t=FC()
+  t$MU=x
+  return(t)
+}
+x=matrix(c(1,2,3,4,5,6,7,0,0,8),2,5)
+fc1=alocarMU(x)
+fc1
+t=FC(MU=x)
+t=FC(MU=matrix(c(1,2,3,4,5,6,7,0,0,8),2,5),SU=matrix(c(1,2,3,4,5,6,7,0,0,100),2,5),SI=matrix(c(1,2,3,4,5,6,7,0,0,9999),2,5))
+class(t)
+t$MU
+names(t)
+t
